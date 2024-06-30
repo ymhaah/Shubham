46541807c8527de40d9af203507030e317476b9a
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2024-06-30 06:12:40 +0300

    bug#1

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Service";
-import Company from "./components/Company";
+// import Company from "./components/Company";
 import Contact from "./components/Contact";
 
 function Home() {
